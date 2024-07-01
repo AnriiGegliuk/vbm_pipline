ca7c61015a89058e1ade53401e839b73bf800f56
AnriiGegliuk <andrii.gegliuk@gmail.com>
2024-07-01 12:54:37 +0900

    tested new segmentation and working as expected

--- segmentation.py
+++ segmentation.py
@@ -1,4 +1,4 @@
 from modalities.segmentation_1 import segment_brain_images
 
-output_files = segment_brain_images('data/alignment/', 'data/TPM/')
+output_files = segment_brain_images('data/segmentation_pipline/', 'data/TPM/')
 print(output_files)
